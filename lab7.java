f2d710d28d8cb0ce457399368f11a72635be6c2c
Aqeel Ahmed <115362175+aqeeljamali786@users.noreply.github.com>
2022-10-28 22:00:06 +0500

    Add files via upload

---
+++ lab7.java
@@ -0,0 +1,21 @@
+class lab7 {
+public static void main(String args[]) {
+String a="aqeel";
+String b="Ahmed";
+System.out.println(" new line escape character");
+System.out.println("aqeel\n"+"Ahmed");
+System.out.println("  backspace character");
+System.out.println("aqeel\b"+"Ahmed");
+System.out.println("  tab escape character");
+System.out.println("aqeel \t"+"Ahmed");
+System.out.println("   single quote escape character");
+System.out.println("aqeel \'"+"Ahmed");
+System.out.println(" double quote escape character");
+System.out.println("aqeel \""+"Ahmed");
+System.out.println("  carriage return quote escape character");
+System.out.println("aqeel \r"+"Ahmed \r");
+System.out.println(" back slash escape character");
+System.out.println("aqeel \\"+"Ahmed");
+}
+}
+ 
